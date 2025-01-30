61cebe05c6ff2d3fa06e1a560c1cb29ad859360a
maypotot <aapotot1@up.edu.ph>
2025-01-30 16:35:50 +0800

    add zigwheels data

--- webscraping/zigwheels_scrape.py
+++ webscraping/zigwheels_scrape.py
@@ -1,3 +1,4 @@
+from operator import ne
 from sys import executable
 import time
 from selenium import webdriver
@@ -13,10 +14,38 @@ import json
 
 final_data = list()
 
+def scroll_down(driver):
+    """A method for scrolling the page."""
+
+    # Get scroll height.
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # Scroll down to the bottom.
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load the page.
+        time.sleep(1)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+
+            break
+
+        last_height = new_height
+        
+        
 def collect_specs(parsed_html: BeautifulSoup, parsed_data: dict, id: str):
     needed_motor_specs = ["Fuel Type", "Transmission Type"]
     specs = parsed_html.find(id="tab-feature" + id + "-panel")
-    li_list = specs.find_all("li")
+    try:
+        li_list = specs.find_all("li")
+    except Exception as e:
+        print(e)
+        return
     for li in li_list:
         details = li.find_all("span")
         key = details[0].text.strip()
@@ -26,7 +55,6 @@ def collect_specs(parsed_html: BeautifulSoup, parsed_data: dict, id: str):
         
 def get_info(url: str, driver: webdriver.Chrome):
     parsed_data = dict()
-    
 
     resp = request.urlopen(url)
     html = resp.read().decode('utf-8')
@@ -38,9 +66,12 @@ def get_info(url: str, driver: webdriver.Chrome):
     #         print(e)
 
     vehicle_info = parsed_html.find(class_="overview-info card-panel")
-    parsed_data["vehicle_name"] = vehicle_info.find("h1").text.strip()
-    parsed_data["vehicle_price"] = vehicle_info.find(class_="vh-price").text.strip()
-    
+    try:
+        parsed_data["vehicle_name"] = vehicle_info.find("h1").text.strip()
+        parsed_data["vehicle_price"] = vehicle_info.find(class_="vh-price").text.strip()[1:]
+    except Exception as e:
+        print(e)
+        return
     
     specs_button = driver.find_element(By.LINK_TEXT, "Specs")
     specs_button.click()
@@ -59,10 +90,11 @@ def get_info(url: str, driver: webdriver.Chrome):
                 specs_button = driver.find_element(By.ID, "tab-feature" + str(i))
                 specs_button.click()    
             except Exception as e:
-                print(e)
+                pass
         
         collect_specs(parsed_html, parsed_data, str(i))
 
+    # put data into json file
     final_data.append(parsed_data)
     json_data = json.dumps(final_data, indent=4)
     
@@ -79,28 +111,42 @@ def navigate():
     thumbnails: list = []
     service = Service(executable_path="chromedriver.exe")
     driver = webdriver.Chrome(service=service)
+    driver.set_window_size(1024, 600)
+    driver.maximize_window()
 
     driver.get("https://www.zigwheels.ph/best-motorcycles")
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "splide05-slide01")))
 
-
-    for i in range(5, 17):
-        concat_str = "splide"
-        if i < 10:
-            concat_str = concat_str + "0" 
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, concat_str + str(i) + "-slide01")))
-        thumbnail = driver.find_element(By.ID, concat_str + str(i) + "-slide01")
-        # print(i)
-        try:
-            thumbnail.click()
-            time.sleep(2)
-            get_url = driver.current_url    
-            get_info(str(get_url), driver)
-            time.sleep(2)
-            driver.execute_script("window.history.go(-2)")
-        except Exception as e:
-            print(e)
-
+    for j in range(2):
+        for i in range(5, 53):
+            concat_str = "splide"
+            if i == 3 or i == 4:
+                continue
+            print(i)
+            scroll_down(driver)
+            if i < 10:
+                concat_str = concat_str + "0" 
+            element_to_find = concat_str + str(i) + "-slide01"
+            try:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, element_to_find)))
+                thumbnail = driver.find_element(By.ID, element_to_find)
+                actions = ActionChains(driver)
+                actions.move_to_element(thumbnail).perform()
+                thumbnail.click()
+                time.sleep(2)
+                get_url = driver.current_url  
+                print(get_url)
+                get_info(str(get_url), driver)
+                time.sleep(2)
+                driver.execute_script("window.history.go(-2)")
+            except Exception as e:
+                print(e)
+        next_button = driver.find_element(By.ID, "view-more-btn")
+        actions = ActionChains(driver)
+        actions.move_to_element(next_button).perform()
+        next_button.click()  
+        
+        
 open('file.txt', 'w').close()
 navigate()
