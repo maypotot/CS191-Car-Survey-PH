a639379df2864f301d7c75361487e857aabf31c1
aishago <125574181+aishago@users.noreply.github.com>
2025-02-01 18:00:59 +0800

    integrate postgre database

--- car_survey/car_survey/settings.py
+++ car_survey/car_survey/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'car_survey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'motor',           
+        'USER': 'postgres',         
+        'PASSWORD': 'password',  
+        'HOST': 'localhost',      
+        'PORT': '5432',           
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
