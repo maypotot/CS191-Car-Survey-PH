981dd855ed9a1d23cf7bb908ee31687bced970cb
aishago <125574181+aishago@users.noreply.github.com>
2025-02-10 20:05:08 +0800

    change password

--- car_survey/car_survey/settings.py
+++ car_survey/car_survey/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vehicle',           
         'USER': 'postgres',         
-        'PASSWORD': 'password',  
+        'PASSWORD': 'i<3sunflowers',  
         'HOST': 'localhost',      
         'PORT': '5432',           
     }
