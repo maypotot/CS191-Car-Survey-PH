4e578f3d300e46ee62fe8939ab346ccf7c385ad7
baamurao <111432334+baamurao@users.noreply.github.com>
2025-03-05 23:55:51 +0800

    Completed PDF scrapers

---
+++ pdfscraping/AFS_scraper.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load the CSV file
+csv_file = "AFS.csv"  # Change this to your file name
+df = pd.read_csv(csv_file)
+
+# Convert to JSON
+json_file = "AFS.json"
+df.to_json(json_file, orient="records", indent=4)  # "records" creates a list of dictionaries
+
+print(f"JSON file saved as {json_file}")
