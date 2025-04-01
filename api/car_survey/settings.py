bcf1d40ad4632eb6a2545f4a13dba513f77f546c
maypotot <aapotot1@up.edu.ph>
2025-04-01 22:54:54 +0800

    misc

--- api/car_survey/settings.py
+++ api/car_survey/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vehicle',           
         'USER': 'postgres',         
-        'PASSWORD': 'i<3sunflowers',  
+        'PASSWORD': 'password',  
         'HOST': 'localhost',      
         'PORT': '5432',           
     }
