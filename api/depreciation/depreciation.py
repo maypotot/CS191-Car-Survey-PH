965cfefcfac1f5c0d07bfb24a214207db8236ce3
maypotot <aapotot1@up.edu.ph>
2025-05-06 20:28:41 +0800

    fix model

--- api/depreciation/depreciation.py
+++ api/depreciation/depreciation.py
@@ -7,7 +7,7 @@ import psycopg2
 
 def predict_depreciation(input_maker: str, input_model: str = "", input_year: int = -1, input_variant: str = "", input_mileage: int = -1, 
                          input_transmission: str = "", input_fuel: str = "", vehicle_type: str = "motors"):
-    conn = psycopg2.connect(host = "localhost", port = 5432, dbname = "vehicle", user = "postgres", password = "password")
+    conn = psycopg2.connect(host = "localhost", port = 5432, dbname = "vehicle", user = "postgres", password = "i<3sunflowers")
     cur = conn.cursor()
 
     cur.execute(f"""
